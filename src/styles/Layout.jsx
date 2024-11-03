bf1b30c7dc33a70668c7e965ee965f85111f9096
Alka Sah <alkasah485@gmail.com>
2024-11-02 23:37:55 -0700

    modification saved

---
+++ src/styles/Layout.jsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const MainLayout = styled.div`
+        padding: 2rem;
+        height: 100%;
+        display: flex;
+        gap: 2rem;
+
+    //     @media (min-width: 768px) {
+    //         flex-direction: row;
+    //        }
+
+    //    @media (min-width: 1200px) {
+    //        padding: 3rem;
+    //      }
+
+`;
+
+export const InnerLayout = styled.div`
+      padding: 2rem 1.5rem;
+      width: 100%;
+    //   @media (min-width: 768px) {
+    //       padding: 2rem;
+    //   }
+
+    //   @media (min-width: 1200px) {
+    //       width: 80%;
+    //       padding: 2.5rem;
+    //   }
+`;
