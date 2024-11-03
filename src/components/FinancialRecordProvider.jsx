34ddfbff94dedb8be47004c01be2b1d9790add17
Alka Sah <alkasah485@gmail.com>
2024-11-02 23:56:26 -0700

    modification saved

--- src/components/FinancialRecordProvider.jsx
+++ src/components/FinancialRecordProvider.jsx
@@ -1,10 +1,37 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import styled from 'styled-components'
+//-------------------------------dependency
+import bg from '../img/bg.png'
+import {MainLayout}  from '../styles/Layout'
+
+const FinancialRecordStyled = styled.div`
+        height: 100vh;
+        background-image: url(${bg});
+        position:relative;
+        main{
+            flex: 1;
+            background: rgba(252, 246, 249, 0.78);
+            border: 3px solid #FFFFFF;
+            backdrop-filter: blur(4.5px);
+            border-radius: 32px;
+            overflow-x: hidden;
+            &::-webkit-scrollbar{
+                width: 0;
+            }
+            }
+`;
+
+
+
 
 const FinancialRecordProvider = () => {
   return (
-    <div>FinancialRecordProvider</div>
+    <FinancialRecordStyled>
+        <MainLayout>
+            
+        </MainLayout>
+    </FinancialRecordStyled>
   )
 }
 
