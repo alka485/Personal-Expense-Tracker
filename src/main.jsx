2851be17605c8bf4f9f68a1d085976c97773baf5
Alka Sah <alkasah485@gmail.com>
2024-11-02 23:30:26 -0700

    modification saved

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {GlobalStyle} from './styles/GlobalStyle.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <StrictMode>
+    <GlobalStyle/>
+    
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
 )
