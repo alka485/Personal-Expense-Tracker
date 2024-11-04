c0ff40ff6ee9984326eb07c15b8560002f2770ff
Alka Sah <alkasah485@gmail.com>
2024-11-04 11:07:46 -0800

    modification saved

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,19 +1,18 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import {Chart as ChartJs, 
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-    ArcElement,
+import React from "react";
+import {Chart as ChartJs,
+      CategoryScale,
+      LinearScale,
+      PointElement,
+      LineElement,
+        Title,
+        Tooltip,
+        Legend,
+        ArcElement,
 } from 'chart.js'
-import {Line} from 'react-chartjs-2'
 import styled from 'styled-components'
 //---------------------------------------------------------dependency
-//import {useGlobalContext} from '../context/globalContext'
+import {useGlobalContext} from '../context/globalContext'
 import  {dateFormat}  from '../utils/dateFormat'
 
 //Register the element
@@ -38,44 +37,9 @@ const ChartStyled = styled.div`
 
 `;
 
-
-
 const Chart = () => {
-    const {incomes , expenses} = useGlobalContext()
-    const data = {
-        labels : incomes.map((inc) => {
-            const {date} = inc
-            return dateFormat(date) 
-        }),
-        datasets : [
-             {
-                label : 'Income',
-                data: [
-                    ...incomes.map((income) => {
-                        const {amount} = income
-                        return amount
-                    })
-                ],
-                backgroundColor: 'green',
-                 tension: .2   //make graphline curve
-             },
-             {
-                label : 'Expenses',
-                data: [
-                    ...expenses.map((expense)=> {
-                        const {amount} = expense
-                        return amount
-                    })
-                ], 
-                backgroundColor: 'red',
-                 tension: .2   //make graphline curve
-             }
-        ]
-    }
   return (
-    <ChartStyled>
-        <Line  data = {data}/>
-    </ChartStyled>
+    <div>Chart</div>
   )
 }
 
